e05c291ab3d0fafd38080c7fc155cae4d171db90
LilyLin16 <726693267@qq.com>
2018-09-28 18:57:19 +0800

    some knowledge about string

--- samples/basic/test_string.py
+++ samples/basic/test_string.py
@@ -16,3 +16,8 @@ print('------------------------------')
 
 print('hello\nrunoob')  # 使用反斜杠(\)+n转义特殊字符
 print(r'hello\nrunoob')  # 在字符串前面添加一个 r，表示原始字符串，不会发生转义
+
+# print 默认输出是换行的，如果要实现不换行需要在变量末尾加上 end=""：
+print('不换行', end="")
+print('end')
+
