176153c71608178a569fa2f5ebffd0dc69fa2dd5
LilyLin16 <726693267@qq.com>
2018-09-28 18:37:25 +0800

    input() function

---
+++ samples/basic/do_input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# -*- coding: utf-8 -*-
+
+name = input("请输入名字:")
+print('Hello,', name)
+age = input("请输入年龄：")
+# input()返回的是str类型
+age = int(age)
+if age <= 12:
+	print('kid')
+elif age <= 19:
+	print('teenager')
+else:
+	print('adult')
